f6c976d209e4f4722a88f3173f2126ee14530de7
Jase Lindgren <jlindgren@perforce.com>
2023-12-08 16:30:00 +0900

    Added history and query functions with arguments Adjusted prompt to get less silly responses. (still a bit silly)

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 import argparse
 
 import openai
+import tiktoken
 from P4 import P4, P4Exception
 
 HISTORY_FILE = Path("data/history.json")
@@ -12,7 +13,9 @@ LATEST_TIMESTAMP = Path("data/latest_timestamp.txt")
 SYSTEM_MESSAGE = """
 You are an assistant to an indie game development team located around the world. You are responsible for making sure everyone is aware of what others are doing by reporting on recent submissions to the Helix Core server.
 
-When you are given a JSON payload of recent changelists, please write a report summarizing what people have been working on. The JSON payload will contain a list of changelists, each of which will have a changelist number, username, time, description, and a list of edited folders. Keep the tone of the reports casual and friendly, but also professional.
+When you are given a JSON payload of recent changelists, please write a report summarizing what people have been working on. The JSON payload will contain a list of changelists, each of which will have a changelist number, username, time (in UTC), description, and a list of edited folders. Keep the tone of the reports casual and friendly, but also professional. Do not give specific details about changelist numbers or times unless they are relevant to the report. If relevant, you could mention how close together some changelists are, but do not give specific times or numbers unless asked.
+
+If you receive a question or instruction instead of a JSON payload, please respond to the best of your ability. For example, if you are asked to give more details on a certain changelist or user, you should respond with a report on that changelist. Or if you are asked to give a summary of what a certain user has been working on, you should respond with a report on that user.
 
 If you have a hard time understanding what people were working on based on their changelist descriptions and edited_folder lists, you can add some friendly suggestions on how they might improve their changelist descriptions to be more descriptive for their teammates. DO NOT do this every time, only when you feel it is necessary to understand what they were working on. Giving specific examples or suggestions is a good way to help them improve their descriptions. Again, DO NOT give suggestions every time, only when the changelist descriptions are difficult to understand.
 
@@ -33,7 +36,7 @@ def main(start_time=None, end_time=None):
         with open("data/history.json", "r") as f:
             previous_messages = json.load(f)
     else:
-        previous_messages = []
+        previous_messages = [{"role": "system", "content": SYSTEM_MESSAGE}]
 
     recent_changelists = get_recent_changelists(start_time, end_time)
 
@@ -45,24 +48,61 @@ def main(start_time=None, end_time=None):
     return ai_response
 
 
-def get_openai_message(previous_messages, recent_changelists):
-    messages = (
-        [{"role": "system", "content": SYSTEM_MESSAGE}]
-        + previous_messages
-        + [
-            {
-                "role": "user",
-                "content": f"```json\n{json.dumps(recent_changelists)}\n```",
-            }
-        ]
-    )
+def ask_query(query):
+    if HISTORY_FILE.exists():
+        with open("data/history.json", "r") as f:
+            previous_messages = json.load(f)
+    else:
+        previous_messages = [{"role": "system", "content": SYSTEM_MESSAGE}]
+    messages = previous_messages + [{"role": "user", "content": query}]
+    messages = truncate_history(messages)
 
     MODEL = "gpt-4-1106-preview"
     response = openai.ChatCompletion.create(
         model=MODEL,
         messages=messages,
-        temperature=0.8,
+        temperature=0.7,
     )
+    print(f'---- Used {response["usage"]["total_tokens"]} tokens ----')
+
+    with open(HISTORY_FILE, "w") as f:
+        json.dump(
+            messages
+            + [
+                {
+                    "role": "assistant",
+                    "content": response["choices"][0]["message"]["content"],
+                }
+            ],
+            f,
+            indent=4,
+        )
+    return response["choices"][0]["message"]["content"]
+
+
+def truncate_history(messages):
+    while num_tokens_from_messages(messages) > 100000:
+        print("Too many tokens, removing oldest message")
+        messages.pop(1)
+    return messages
+
+
+def get_openai_message(previous_messages, recent_changelists):
+    messages = previous_messages + [
+        {
+            "role": "user",
+            "content": f"```json\n{json.dumps(recent_changelists)}\n```",
+        }
+    ]
+    messages = truncate_history(messages)
+
+    MODEL = "gpt-4-1106-preview"
+    response = openai.ChatCompletion.create(
+        model=MODEL,
+        messages=messages,
+        temperature=0.7,
+    )
+    print(f'---- Used {response["usage"]["total_tokens"]} tokens ----')
 
     with open(HISTORY_FILE, "w") as f:
         json.dump(
@@ -138,6 +178,27 @@ def get_recent_changelists(previous_datetime=None, current_datetime=None):
     ]
 
 
+def num_tokens_from_messages(messages, model="gpt-4"):
+    """Return the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+
+    tokens_per_message = 3
+    tokens_per_name = 1
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
+
+
 def extract_unique_directories(file_paths):
     unique_dirs = set()
 
@@ -156,9 +217,12 @@ def extract_unique_directories(file_paths):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--history", help="Start date in YYYY/MM/DD:HH:MM:SS format")
-    if parser.parse_args().history:
-        start_date = parser.parse_args().history
-        start_date = datetime.strptime("2023/11/05:00:00:00", "%Y/%m/%d:%H:%M:%S")
+    parser.add_argument("--query", "-q", help="A plaintext query to ask the LLM.")
+
+    args = parser.parse_args()
+
+    if args.history:
+        start_date = datetime.strptime(args.history, "%Y/%m/%d:%H:%M:%S")
         past_dates = [start_date.strftime("%Y/%m/%d:%H:%M:%S")]
         while start_date < datetime.utcnow():
             start_date += timedelta(days=1)
@@ -171,6 +235,9 @@ if __name__ == "__main__":
             if res:
                 all_responses.append(res)
                 print(res)
+    elif args.query:
+        res = ask_query(args.query)
+        print(res)
     else:
         res = main()
         print(res)
